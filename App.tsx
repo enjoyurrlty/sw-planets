df3946a509cbedc22e7b03e8433be447fcc01572
Artur Khafizov <ukkc42@ya.ru>
2022-08-28 23:45:33 +0200

    Add router WIP

--- App.tsx
+++ App.tsx
@@ -1,16 +1,33 @@
 import React from 'react';
-import {NativeBaseProvider, Flex} from 'native-base';
+import {NativeBaseProvider, Flex, Text} from 'native-base';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Planets from './src/screens/Planets';
 import AppBar from './src/components/AppBar';
 
+const Stack = createNativeStackNavigator();
+
+function Test() {
+  return <Text>Test</Text>;
+}
+
 const App = () => {
   return (
-    <NativeBaseProvider>
-      <Flex flexGrow={1}>
-        <AppBar />
-        <Planets />
-      </Flex>
-    </NativeBaseProvider>
+    <NavigationContainer>
+      <NativeBaseProvider>
+        <Flex flexGrow={1}>
+          <AppBar />
+          <Stack.Navigator>
+            <Stack.Screen
+              name="Planets"
+              component={Planets}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen name="Profile" component={Test} />
+          </Stack.Navigator>
+        </Flex>
+      </NativeBaseProvider>
+    </NavigationContainer>
   );
 };
 export default App;
